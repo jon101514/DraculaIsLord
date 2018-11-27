7010b64faa2bd2d7d8abbb28c7e06a5d376ef4ae
vdd7204 <vdd7204@IGM2570-25>
2018-11-27 10:01:17 -0500

    Delta time now calculated at the top of Update

--- Pong/AppClass.cpp
+++ Pong/AppClass.cpp
@@ -68,6 +68,10 @@ void Application::InitVariables(void)
 }
 void Application::Update(void)
 {
+
+	static uint uClock = m_pSystem->GenClock(); //generate a new clock for that timer
+	int deltaTime = m_pSystem->GetDeltaTime(uClock); //get the delta time for that timer
+
 	//Update the system so it knows how much time has passed since the last call
 	m_pSystem->Update();
 
