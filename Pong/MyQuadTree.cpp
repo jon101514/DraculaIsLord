c664225791d5fb5bba9c037b3b0cf9c705c2671f
RainyAngel <charlink22@gmail.com>
2018-12-02 21:06:19 -0500

    Added functions

--- Pong/MyQuadTree.cpp
+++ Pong/MyQuadTree.cpp
@@ -2,11 +2,44 @@
 
 
 
+void MyQuadTree::Init()
+{
+}
+
+void MyQuadTree::Delete()
+{
+}
+
 MyQuadTree::MyQuadTree()
 {
 }
 
 
 MyQuadTree::~MyQuadTree()
+{
+}
+
+void MyQuadTree::IsColliding()
+{
+}
+
+void MyQuadTree::Display()
+{
+}
+
+void MyQuadTree::Subdivide()
+{
+}
+
+bool MyQuadTree::IsLeaf()
+{
+	return false;
+}
+
+void MyQuadTree::ConstructList(int maxLevel, int ideal_Count)
+{
+}
+
+void MyQuadTree::KillBranches()
 {
 }
--- Pong/MyQuadTree.h
+++ Pong/MyQuadTree.h
@@ -2,7 +2,15 @@
 class MyQuadTree
 {
 public:
+	void Init();
+	void Delete();
 	MyQuadTree();
 	~MyQuadTree();
+	void IsColliding();
+	void Display();
+	void Subdivide();
+	bool IsLeaf();
+	void ConstructList(int maxLevel, int ideal_Count);
+	void KillBranches();
 };
 
