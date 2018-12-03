9cbeec3ba6b4fa74ac70c722bbf5f1f410674fe0
Vincent-DiCocco <vincent.dicocco@hotmail.com>
2018-12-02 20:57:21 -0500

    arguments

--- Pong/MyQuadTree.h
+++ Pong/MyQuadTree.h
@@ -1,8 +1,29 @@
 #pragma once
+#include <vector>
+#include "MyRigidBody.h"
+
+namespace Simplex
+{
 class MyQuadTree
 {
+	uint m_uID = 0;
+
+	MyQuadTree* m_pChildren[8];
+
+	float m_fSize = 0.0f;
+
+	std::vector<MyRigidBody> m_containedObjects;
+
+	vector3 m_v3Center = vector3(0.0f);
+	vector3 m_v3Min = vector3(0.0f);
+	vector3 m_v3Max = vector3(0.0f);
+
+	MyQuadTree* m_pRoot = nullptr;
+	std::vector<MyQuadTree*> m_lChild;
+
 public:
 	MyQuadTree();
 	~MyQuadTree();
 };
+}
 
